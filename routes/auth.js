9299a9f4556af5cce4cd1d965dcc202f20b62ac6
apimento <98665437+apimento@users.noreply.github.com>
2022-03-11 17:57:24 -0500

    auth controllers sign up and sign in ejs

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,16 @@ const{body} = require("express-validator");
 
 const authCntrl = require("../controllers/auth"); 
 
-router.get("/auth/signup", authCntrl.auth_signup_get);
-router.post("/auth/signup",[
+router.get("/auth/signUp", authCntrl.auth_signup_get);
+router.post("/auth/signUp",[
     body('userName').isLength({min:2}).notEmpty().withMessage("firtName cannot be null"),
     body('emailAddress').isEmail(), 
     body('password').isLength({min:6}).withMessage("Password must be at least 6 charactes long")
 ], authCntrl.auth_signup_post);
 
 
-router.get("/auth/signin", authCntrl.auth_signin_get);  
-router.post("/auth/signin", authCntrl.auth_signin_post); 
+router.get("/auth/signIn", authCntrl.auth_signin_get);  
+router.post("/auth/signIn", authCntrl.auth_signin_post); 
 
 
 module.exports = router;
